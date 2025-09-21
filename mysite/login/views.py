2b80f6bc8f0bb76100089e9191b402ff037496c2
njustinbunos <justinjairebanzuela.bunos25@bicol-u.edu.ph>
2025-09-21 23:07:12 +0800

    ...

--- mysite/login/views.py
+++ mysite/login/views.py
@@ -1,12 +1,39 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 def index(request):
     if not request.user.is_authenticated: 
-        return redirect("login")
-    return render(request, "index.html")
+        return redirect('login')
+    return render(request, 'templates/index.html')
 
 def login_view(request):
-    return render(request, "login.html", {"title":"Login"})
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('index')
+        else:
+            messages.error(request, 'Invalid username or password')
+    return render(request, 'templates/login.html',{'title':'Log in'})
 
 def register(request):
-    return render(request, "register.html",{"title":"Register"})
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('index')
+        
+    else:
+        form = UserCreationForm()
+    return render(request, "register.html", {"form": form, "title": "Register"})
+
+def logout_view(request):
+    logout(request)
+    return redirect("login")
+
+
