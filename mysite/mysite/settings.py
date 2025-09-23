c2370b76dd85bb7030a8edb5bf90eb0d6b24fddb
njustinbunos <justinjairebanzuela.bunos25@bicol-u.edu.ph>
2025-09-23 08:46:46 +0800

    rename: app login to main

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'login.apps.LoginConfig',
+    'main.apps.MainConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
